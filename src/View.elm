e2347624a4beddfc2c03a8bee636072054a9006e
Lesley <lesleylai@lesleylai.info>
2019-04-15 13:39:17 -0600

    View for the agent selection

--- src/View.elm
+++ src/View.elm
@@ -3,12 +3,16 @@ module View exposing (view)
 import Model exposing (GameState, Piece(..), Position, Winner(..),
                            Cell, Board, Row, Model, Move)
 
+import FontAwesome.Icon
+import FontAwesome.Solid
+
 import Array exposing (Array)
 import Grid
-import Html exposing (Html, article, section, text, div, h1, img, p)
+import Html exposing (Html, article, span, section, text, div, h1, img, p)
 import Html.Attributes exposing (class, classList, style, src)
 import Html.Events exposing (onClick)
 import Set exposing (Set)
+import Svg.Attributes
 import Update exposing (Msg(..), countPieces, init, update)
 
 type alias MovesSet = Set Position
@@ -61,15 +65,33 @@ currentPlayerView state =
         Just WhiteWin -> div [] [ text <| "White Win"]
         Just Tie -> div [] [ text <| "Tie"]
 
+
+agentIcons: Html Msg
+agentIcons =
+    div [ classList [("agent-icons", True)] {-onClick Decrement-} ]
+                    [ FontAwesome.Icon.viewStyled
+                          [Svg.Attributes.class "agent-icon"
+                          , Svg.Attributes.class "agent-icon-inactive"]
+                          FontAwesome.Solid.robot
+                    ,
+                        FontAwesome.Icon.viewStyled
+                            [Svg.Attributes.class "agent-icon"
+                            , Svg.Attributes.class "agent-icon-active"]
+                            FontAwesome.Solid.user ]
+
 scoresView: GameState -> Html Msg
 scoresView state =
     let (whiteCount, blackCount) = countPieces state.board in
     article []
         [
-         p [] [ text <| "White: " ++ (String.fromInt whiteCount) ]
-        ,p [] [ text <| "Black: " ++ (String.fromInt blackCount) ]
+         p [ classList [("agent", True)] ]
+             [ agentIcons,
+                text <| "White (Human) Score: " ++ (String.fromInt whiteCount) ]
+        ,p [ classList [("agent", True)] ]
+            [ agentIcons,
+                text <| "Black (Human) Score: " ++ (String.fromInt blackCount) ]
         ]
-                  
+
 
 view : Model -> Html Msg
 view model =
