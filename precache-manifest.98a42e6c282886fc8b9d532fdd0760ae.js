0b241fe63b42b597562afca79348f56ec72ce09a
Lesley Lai <lesley@lesleylai.info>
2020-11-24 00:47:05 -0700

    Updates

---
+++ precache-manifest.98a42e6c282886fc8b9d532fdd0760ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/Reversi-Elm-V2/favicon.ico"
+  },
+  {
+    "revision": "c24f29b6cf7c928cfa290653cac0a29d",
+    "url": "/Reversi-Elm-V2/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/Reversi-Elm-V2/logo.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Reversi-Elm-V2/main.js"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/Reversi-Elm-V2/manifest.json"
+  },
+  {
+    "revision": "c5b95537ed8986d8d02f",
+    "url": "/Reversi-Elm-V2/static/css/main.8c80f93a.chunk.css"
+  },
+  {
+    "revision": "c5b95537ed8986d8d02f",
+    "url": "/Reversi-Elm-V2/static/js/main.c5b95537.chunk.js"
+  },
+  {
+    "revision": "795894402c7095eb07ff",
+    "url": "/Reversi-Elm-V2/static/js/runtime~main.79589440.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/Reversi-Elm-V2/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
